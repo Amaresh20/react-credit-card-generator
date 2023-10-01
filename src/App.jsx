9d2b49d41ab96915dd79dd84aa42681f11753c5b
Amaresh20 <sahooa218@gmail.com>
2023-10-01 12:46:57 +0530

    boilerplate react setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+     <h1>credit card generator</h1>
+    </div>
+  )
+}
+
+export default App
